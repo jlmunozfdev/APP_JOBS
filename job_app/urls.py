65b98ec6ec07c14c8f3f34837df13f549c26b87c
Jorge  Muñoz <104048177+jlmunozfdev@users.noreply.github.com>
2025-01-21 21:23:03 -0500

    update urls

--- job_app/urls.py
+++ job_app/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'), # new
+    path('', views.home, name='home'),
+    path('add/', views.job_create, name='job_create'),
+    path('edit/<int:pk>/', views.job_update, name='job_update'),
+    path('delete/<int:pk>/', views.job_delete, name='job_delete'),
 ]
 
 
