d0f50a37e0b97eed9f7ff6043084ab2c63963e92
Jorge  Muñoz <104048177+jlmunozfdev@users.noreply.github.com>
2025-01-21 21:22:24 -0500

    update views

--- job_app/views.py
+++ job_app/views.py
@@ -1,5 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import JobApplication
+from .forms import JobApplicationForm
 
-# Create your views here.
+
+def index(request):
+     return render(request, 'index.html')
+
 def home(request):
-    return render(request, 'home.html') # new
+    jobs = JobApplication.objects.all()
+    return render(request, 'home.html', {'jobs': jobs})
+
+def job_create(request):
+    if request.method == 'POST':
+        form = JobApplicationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = JobApplicationForm()
+    return render(request, 'job_form.html', {'form': form, 'action': 'Add'})
+
+def job_update(request, pk):
+    job = get_object_or_404(JobApplication, pk=pk)
+    if request.method == 'POST':
+        form = JobApplicationForm(request.POST, instance=job)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = JobApplicationForm(instance=job)
+    return render(request, 'job_form.html', {'form': form, 'action': 'Edit'})
+
+def job_delete(request, pk):
+    job = get_object_or_404(JobApplication, pk=pk)
+    if request.method == 'POST':
+        job.delete()
+        return redirect('home')
+    return render(request, 'job_confirm_delete.html', {'job': job})
