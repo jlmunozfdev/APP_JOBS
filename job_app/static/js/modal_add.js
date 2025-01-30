6d48b82e9d00a689c29ca247e39639af35eb82aa
Jorge  Muñoz <104048177+jlmunozfdev@users.noreply.github.com>
2025-01-30 11:29:42 -0500

    Add modal New Job in Home

---
+++ job_app/static/js/modal_add.js
@@ -0,0 +1,7 @@
+function openAddModal() {
+    document.getElementById('addModalJob').classList.remove('hidden');
+}
+
+function closeAddModal() {
+    document.getElementById('addModalJob').classList.add('hidden');
+}
