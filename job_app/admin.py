a3e6a8eef49587c8d33ee223354c39337b091022
Jorge  Muñoz <104048177+jlmunozfdev@users.noreply.github.com>
2025-02-04 16:20:05 -0500

    Add class_JobAdmin in admin

--- job_app/admin.py
+++ job_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import JobApplication
 
 # Register your models here.
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('job_title', 'company_name', 'application_date', 'closing_date', 'status',)
+    search_fields = ('job_title', 'company_name', 'application_date', 'closing_date', 'status',)
+    list_filter = ('status',)
+    
+
+admin.site.register(JobApplication, JobAdmin)
